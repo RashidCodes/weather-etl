837556ef0caa6f7b649c9666a966a9915e56c8b8
Mohammed Rashid <rashidmincan@gmail.com>
2022-09-20 21:29:19 +1000

    Delete config.template.sh

--- weather/config.template.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#export PYTHONPATH=$(pwd)
-export api_key="1fcc4dd314cad5316807c375da396d02"
-export db_user="postgres"
-export db_password=""
-export db_server_name="localhost"
-export db_database_name="postgres"
